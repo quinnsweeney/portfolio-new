38890fadab2f02d0c59b4d2683ad0ca16547eefa
quinnsweeney11 <qasweeney@crimson.ua.edu>
2025-12-11 12:24:17 -0600

    Updated about me page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,6 +2,7 @@ import Box from "@mui/joy/Box";
 import Typography from "@mui/joy/Typography";
 import Sheet from "@mui/joy/Sheet";
 import Avatar from "@mui/joy/Avatar";
+import { Link } from "@mui/joy";
 
 function AboutPage() {
   return (
@@ -24,23 +25,29 @@ function AboutPage() {
         }}
       >
         <Avatar src="headshot.jpeg" sx={{ "--Avatar-size": "150px" }} />
-        <Typography level="h1" component="h1">
+        {/* <Typography level="h1" component="h1">
           About Me
-        </Typography>
+        </Typography> */}
         <Typography level="body-lg" sx={{ maxWidth: "600px" }}>
-          Hi, I'm Quinn! I grew up in Denver, Colorado and am currently an
-          undergraduate student at the University of Alabama majoring in MIS
-          with a minor in Computer Science. I love to ski, watch sports, and
-          read - Brandon Sanderson and Matt Dinniman are my favorites!
+          Hi, I'm Quinn! I grew up in Denver, Colorado and am an Alumni from the
+          University of Alabama with Bachelor's in MIS & CS. I love to ski,
+          watch sports, and read - Brandon Sanderson and Matt Dinniman are my
+          favorites!
         </Typography>
         <Typography level="body-lg" sx={{ maxWidth: "600px" }}>
           I've been fascinated with computers for my entire life, and it lead me
-          to teach myself how to code at a relatively young age. From there I
-          got an apprenticeship doing web development at Pinnacol Assurance -
-          the largest workers compensation insurance company in Colorado. I'm
-          currently working on a full-stack web app to assist in ski resort
-          navigation. I love building things that are useful and fun, and I hope
-          to make a positive impact on the world through technology.
+          to teach myself how to code at a relatively young age. I'm currently
+          working on{" "}
+          <Link
+            href="http://localhost:5173/project/project-1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            SkiFinder
+          </Link>
+          , a web & mobile app to assist in ski resort navigation. I love
+          building things that are useful and fun, and I hope to make a positive
+          impact on the world through technology.
         </Typography>
       </Box>
     </Sheet>
